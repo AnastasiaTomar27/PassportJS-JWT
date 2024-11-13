975d502ad880723c3e37513008b59d10aa57fb48
AnastasiaTomar27 <anastasia.tomar@edu.keuda.fi>
2024-11-13 16:04:56 +0200

    deleting app.use(cookieParser("jwt learning"));

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ const PORT = process.env.PORT || 3000;
 
 // middleware
 app.use(express.json());
-app.use(cookieParser("jwt learning")); // it makes the cookies easily readable from the request.cookies
+//app.use(cookieParser("jwt learning")); // it makes the cookies easily readable from the request.cookies
 
 app.use(passport.initialize()); //  import and initialize Passport globally so that it's available across all routes
 
